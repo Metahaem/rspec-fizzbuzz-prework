835c2c949736cd29dcd151f483b77676e4a5abdb
Tom Rowson <tom.rowson@gmail.com>
2018-10-01 15:06:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
-def fizzbuzz(entry")
- if entry % 3 == 0 && (entry % 5 != 0)
+def fizzbuzz(entry)
+ if (entry % 3 == 0) && (entry % 5 != 0)
   return "Fizz"
  else if (entry % 5 == 0) && (entry % 3 != 0)
   return "Buzz"
